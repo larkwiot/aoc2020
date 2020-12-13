dfe1c903064175001207fe5742f5b44e0dc06f67
larkwiot <ltfragor@gmail.com>
2020-12-13 13:02:25 -0500

    day5p1 actually using my optimization

--- day5/part1.h
+++ day5/part1.h
@@ -21,18 +21,32 @@ int compute_id(std::string pass) {
     return (nrow * 8) + ncol;
 }
 
+bool cmp_char(const char lhs, const char rhs) {
+    static std::string ab = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static std::string tr = "ABCDEFGHIJKRMNOPQLSTUVWXYZ";
+    
+    auto lhs_tr = tr[ab.find(lhs)];
+    auto rhs_tr = tr[ab.find(rhs)];
+
+    return lhs_tr < rhs_tr;
+}
+
+bool cmp_str(const std::string& lhs, const std::string& rhs) {
+    bool result = std::lexicographical_compare(
+        lhs.begin(), lhs.end(),
+        rhs.begin(), rhs.end(),
+        cmp_char    
+    );
+
+    return result;
+}
+
 bool solve(std::string fn) {
     auto lines = read_file_lines(fn);
 
-    std::sort(lines.begin(), lines.end());
+    std::sort(lines.begin(), lines.end(), cmp_str);
 
-    std::unordered_set<int> ids {};
-
-    for (auto l : lines) {
-        ids.emplace(compute_id(l));
-    }
-
-    int max_id = *(std::max_element(ids.begin(), ids.end()));
+    int max_id = compute_id(lines.at(0));
 
     std::cout << "[-] finish: max id: " << max_id << "\n";
 
